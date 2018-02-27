8ee28ca745f2c02bbaf275cea54bba251503508c
Juri <juri.strumpflohner@gmail.com>
2018-02-27 21:02:00 +0100

    fix: remove ng-style private exports

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,3 @@
 export { NgxTabsLibModule } from './src/ngx-tabs-lib.module';
-export { TabsComponent as ɵTabsComponent } from './src/tabs.component';
-export { TabComponent as ɵTabComponent } from './src/tab.component';
+export { TabsComponent } from './src/tabs.component';
+export { TabComponent } from './src/tab.component';
