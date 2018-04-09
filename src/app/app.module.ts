2886f24f296c344acc79036af435450c42601d1a
Juri <juri.strumpflohner@gmail.com>
2018-04-09 23:28:16 +0200

    WIP: experimenting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxTabsLibModule } from 'projects/ngx-tabs-lib';
-// import { NgxTabsLibModule } from '@juristr/ngx-tabs-lib';
+// import { NgxTabsLibModule } from 'projects/ngx-tabs-lib';
+// import { NgxTabsLibModule } from 'projects/ngx-tabs-lib/src/public_api';
+import { NgxTabsLibModule } from '@juristr/ngx-tabs-lib';
 
 @NgModule({
   declarations: [AppComponent],
