a0a5b4a406f002399534aca6b0fe8f8811c59a07
Juri <juri.strumpflohner@gmail.com>
2018-04-20 21:54:00 +0200

    build: adjust metadata setting

---
+++ tools/build/set-metadata.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+'use strict';
+
+import { writeFile } from 'fs';
+import { getPackages } from './utils';
+
+async function main() {
+  const ngxsJson = require('../../package.json');
+  const keysToCopy = ['version', 'repository', 'keywords', 'author', 'contributors', 'license', 'bugs', 'homepage'];
+
+  const packages = getPackages();
+  for (const pack of packages) {
+    const packPath = `${pack.buildPath}/package.json`;
+    const packPackage = require(packPath);
+
+    // copy all meta data from the root package.json into all packages
+    for (const key of keysToCopy) {
+      packPackage[key] = ngxsJson[key];
+    }
+
+    // set all the packages peerDependencies to be the same as root package.json version
+    // for (const p of packages) {
+    //   if (packPackage.peerDependencies[p.packageName]) {
+    //     packPackage.peerDependencies[p.packageName] = ngxsJson.version;
+    //   }
+    // }
+
+    // save the package file after we have updated the keys and peerDependencies
+    await writeFile(packPath, JSON.stringify(packPackage, null, 2), err => {
+      if (err) {
+        console.error('Write failed!');
+      }
+    });
+  }
+
+  console.log(`package version set to ${ngxsJson.version}`);
+}
+
+main();
